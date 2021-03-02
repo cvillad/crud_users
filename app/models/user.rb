4d323ee44ed7965e40f6184664fccbc038507c51
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-02 18:16:35 -0500

    fix rails version, modify use of variants

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :social_networks, dependent: :destroy
   accepts_nested_attributes_for :social_networks, allow_destroy: true
-  has_one_attached :image, dependent: :destroy  do |attachable|
-      attachable.variant :thumb, resize: "50x50"
-      attachable.variant :profile, resize: "150x150"
-  end
+  has_one_attached :image, dependent: :destroy 
   validates :image, content_type: ["image/png", "image/jpg", "image/jpeg"]
   validates :first_name, presence: true, length: { maximum: 25 }
   validates :last_name, presence: true, length: { maximum: 25 }
   validates :username, presence: true, uniqueness: true
   validates :description, presence: true
+
+  def profile_img
+    image.variant(resize: "150x150")
+  end
+
+  def thumb_img
+    image.variant(resize: "50x50")
+  end
 end

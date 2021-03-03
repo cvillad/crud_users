e5ee34c711c7ea93ceb57dde09dea5936eba3b7b
Carlos Villa <cvillad@uninorte.edu.co>
2021-03-03 17:11:10 -0500

    fix destroy image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   def destroy_image
     @user = User.find(params[:user_id])
     @user.image.purge
-    redirect_to edit_user_registration
+    redirect_to edit_user_registration_path
   end
 
 end
